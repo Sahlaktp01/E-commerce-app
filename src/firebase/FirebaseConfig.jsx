f5febaccf06251e57a951c74d68ce45f27fa9513
Sahla K <sahlaktp01@gmail.com>
2026-01-15 03:18:57 +0530

    Prepare for deployment

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDueLk1LoTY7ixwpI7VARSgoYyEXxKKrGw",
-  authDomain: "myfirstapp-f4539.firebaseapp.com",
-  projectId: "myfirstapp-f4539",
-  storageBucket: "myfirstapp-f4539.firebasestorage.app",
-  messagingSenderId: "180022992966",
-  appId: "1:180022992966:web:e03364decb653f85acaa47"
+ apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
